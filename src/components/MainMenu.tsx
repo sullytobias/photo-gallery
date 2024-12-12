ff6d3e5130d17d3af151c96e202c5b69a1c1aed1
Sully <147931276+sullytobias@users.noreply.github.com>
2024-12-12 17:09:06 +0100

    Add Some Transitions & Shapes

--- src/components/MainMenu.tsx
+++ src/components/MainMenu.tsx
@@ -1,5 +1,3 @@
-import { useThree } from "@react-three/fiber";
-import { useEffect } from "react";
 import Slider from "./Slider";
 
 type MainMenuProps = {
@@ -7,18 +5,7 @@ type MainMenuProps = {
 };
 
 const MainMenu: React.FC<MainMenuProps> = ({ onEnterGallery }) => {
-    const { camera } = useThree();
-
-    useEffect(() => {
-        camera.position.set(5, 5, 5);
-        camera.lookAt(0, 0, 0);
-    }, [camera]);
-
-    return (
-        <group>
-            <Slider onEnterGallery={onEnterGallery} />
-        </group>
-    );
+    return <Slider onEnterGallery={onEnterGallery} />;
 };
 
 export default MainMenu;
--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { motion } from "framer-motion-3d";
 import { Text } from "@react-three/drei";
 
 type SliderProps = {
@@ -17,37 +18,120 @@ const Slider: React.FC<SliderProps> = ({ onEnterGallery }) => {
             (prev) => (prev - 1 + galleries.length) % galleries.length
         );
 
-    return (
-        <group>
-            <mesh position={[-2, 0, 0]} onClick={handlePrev}>
-                <boxGeometry args={[0.5, 0.5, 0.5]} />
-                <meshStandardMaterial color="blue" />
-            </mesh>
+    const getNextIndex = () => (currentIndex + 1) % galleries.length;
+    const getPrevIndex = () =>
+        (currentIndex - 1 + galleries.length) % galleries.length;
 
-            <group position={[0, 0, 0]}>
+    return (
+        <motion.group>
+            {/* Left Navigation */}
+            <motion.group
+                scale={0.6}
+                position={[-1.3, 0, 0]}
+                whileHover={{ scale: 0.7 }}
+                onClick={handlePrev}
+            >
+                {/* Previous Gallery Title */}
+                <motion.group
+                    key={galleries[getPrevIndex()]}
+                    initial={{ scale: 0, x: -1 }}
+                    animate={{ scale: 1, x: 0 }}
+                    exit={{ scale: 0, x: 0 }}
+                >
+                    <Text
+                        position={[0, 0.6, 0]}
+                        fontSize={0.12}
+                        anchorX="center"
+                        anchorY="middle"
+                        color="blue"
+                    >
+                        {galleries[getPrevIndex()]}
+                    </Text>
+                </motion.group>
+
+                <mesh position={[0.3, 0, 0]} rotation={[0, 0, Math.PI / 2]}>
+                    <cylinderGeometry args={[0.08, 0.08, 1.2, 16]} />
+                    <meshStandardMaterial color="blue" />
+                </mesh>
+                <mesh position={[-0.4, 0, 0]} rotation={[0, 0, Math.PI / 2]}>
+                    <coneGeometry args={[0.2, 0.4, 16]} />
+                    <meshStandardMaterial color="blue" />
+                </mesh>
+            </motion.group>
+
+            {/* Main Box */}
+            <motion.group
+                key={currentIndex}
+                position={[0, 0, 0]}
+                initial={{ scale: 0, y: 1 }}
+                animate={{ scale: 1, y: 0 }}
+                exit={{ scale: 0, y: 0 }}
+                transition={{ type: "spring", stiffness: 180, damping: 12 }}
+            >
+                {/* Gallery Title */}
                 <Text
-                    position={[0, 1.2, 0]}
-                    fontSize={0.5}
+                    position={[0, 0.3, 0.51]}
+                    fontSize={0.15}
                     anchorX="center"
                     anchorY="middle"
                     color="black"
                 >
                     {galleries[currentIndex]}
                 </Text>
-                <mesh
-                    position={[0, 0, 0]}
-                    onClick={() => onEnterGallery(galleries[currentIndex])}
-                >
+
+                {/* Main Gallery Box */}
+                <mesh position={[0, 0, 0]}>
                     <boxGeometry args={[1, 1, 1]} />
                     <meshStandardMaterial color="red" />
                 </mesh>
-            </group>
 
-            <mesh position={[2, 0, 0]} onClick={handleNext}>
-                <boxGeometry args={[0.5, 0.5, 0.5]} />
-                <meshStandardMaterial color="blue" />
-            </mesh>
-        </group>
+                {/* Door */}
+                <mesh
+                    onClick={() => onEnterGallery(galleries[currentIndex])}
+                    position={[0, -0.25, 0.51]}
+                    scale={0.2}
+                >
+                    <planeGeometry args={[1, 2]} />
+                    <meshStandardMaterial color="white" />
+                </mesh>
+            </motion.group>
+
+            {/* Right Navigation */}
+            <motion.group
+                scale={0.6}
+                position={[1.3, 0, 0]}
+                whileHover={{ scale: 0.7 }}
+                onClick={handleNext}
+            >
+                {/* Next Gallery Title */}
+                <motion.group
+                    key={galleries[getNextIndex()]}
+                    initial={{ scale: 0, x: 1 }}
+                    animate={{ scale: 1, x: 0 }}
+                    exit={{ scale: 0, x: 0 }}
+                >
+                    <Text
+                        position={[0, 0.6, 0]}
+                        fontSize={0.12}
+                        anchorX="center"
+                        anchorY="middle"
+                        color="blue"
+                    >
+                        {galleries[getNextIndex()]}
+                    </Text>
+                </motion.group>
+
+                <mesh position={[-0.3, 0, 0]} rotation={[0, 0, Math.PI / 2]}>
+                    <cylinderGeometry args={[0.08, 0.08, 1.2, 16]} />
+                    <meshStandardMaterial color="blue" />
+                </mesh>
+
+                <mesh position={[0.4, 0, 0]} rotation={[0, 0, -Math.PI / 2]}>
+                    <coneGeometry args={[0.2, 0.4, 16]} />
+                    <meshStandardMaterial color="blue" />
+                </mesh>
+            </motion.group>
+        </motion.group>
     );
 };
 
